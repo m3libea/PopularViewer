aa3b156c2bb5d2b2781d13a0f1b0fff77a9603c4
m3libea <rociorommor@gmail.com>
2015-02-04 15:49:27 -0800

    Model Data Created

---
+++ app/src/main/java/com/m3libea/popularviewer/InstagramPhoto.java
@@ -0,0 +1,15 @@
+package com.m3libea.popularviewer;
+
+/**
+ * Created by melibea on 2/3/15.
+ */
+public class InstagramPhoto {
+
+    public String username;
+    public String caption;
+    public String imageUrl;
+    public int imageHeight;
+    public int likesCount;
+
+
+}
--- app/src/main/java/com/m3libea/popularviewer/PhotosActivity.java
+++ app/src/main/java/com/m3libea/popularviewer/PhotosActivity.java
@@ -2,18 +2,97 @@ package com.m3libea.popularviewer;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public class PhotosActivity extends ActionBarActivity {
+
+    public static final String CLIENT_ID = "1c349f0542194fa7b4b083d034b70901";
+    private ArrayList<InstagramPhoto> photos;
+    private InstagramPhotosAdapter aPhotos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photos);
+        photos = new ArrayList<>();
+        //Create the adapter linking to the source
+        aPhotos = new InstagramPhotosAdapter(this, photos);
+        //Find the ListView from the layout
+        ListView lvphotos = (ListView)findViewById(R.id.lvPhotos);
+        //Set the adapter binding into the ListView
+        lvphotos.setAdapter(aPhotos);
+        fetchPopularPhotos();
     }
 
+    public void fetchPopularPhotos() {
+
+        String url = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
+        //Create the network client
+        AsyncHttpClient client = new AsyncHttpClient();
+        //Trigger the GET request
+        client.get(url, null, new JsonHttpResponseHandler(){
+            //onSuccess
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                //Expecting a JSON object
+                // { "data" => [X] => "user" => "username"}
+                // { "data" => [X] => "caption" => "text"}
+                // { "data" => [X] => "images" => "standard_resolution" => "url"}
+                // { "data" => [X] => "images" => "standard_resolution" => "height"}
+                // { "data" => [X] => "likes" => "count"}
+
+                JSONArray photosJSON = null;
+
+                try {
+                    photosJSON = response.getJSONArray("data");
+                    //iterate array
+
+                    for(int i =0; i < photosJSON.length(); i++){
+                        JSONObject photoJSON = photosJSON.getJSONObject(i);
+                        //decode attributes
+                        InstagramPhoto photo = new InstagramPhoto();
+                        photo.username = photoJSON.getJSONObject("user").getString("username");
+                        photo.caption = photoJSON.getJSONObject("caption").getString("text");
+                        photo.imageUrl = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
+                        photo.imageHeight = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
+                        photo.likesCount = photoJSON.getJSONObject("likes").getInt("count");
+                        //Add to photos array
+
+                        photos.add(photo);
+
+
+                    }
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+                //callback
+                aPhotos.notifyDataSetChanged();
+            }
+
+            //onFailure
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
